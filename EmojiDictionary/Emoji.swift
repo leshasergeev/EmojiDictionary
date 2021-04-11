85508e756e8186fe29f683e65b666681588baffe
Alexey Sergeev <aleksei_s@bk.ru>
2021-04-11 12:36:39 +0300

    Project was created and "Emoji" file added.

---
+++ EmojiDictionary/Emoji.swift
@@ -0,0 +1,16 @@
+//
+//  Emoji.swift
+//  EmojiDictionary
+//
+//  Created by Алексей Сергеев on 11.04.2021.
+//  Copyright © 2021 Алексей Сергеев. All rights reserved.
+//
+
+import Foundation
+
+struct Emoji {
+    var symbol:String
+    var name: String
+    var description: String
+    var usage: String
+}
